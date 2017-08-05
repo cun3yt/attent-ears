775aebeb15a6ef29e417d3342ee539b258804435
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-08-04 22:45:50 -0700

    Syncing clients with keep_in_sync flag on

--- scripts/extract_google_calendar.py
+++ scripts/extract_google_calendar.py
@@ -5,11 +5,15 @@ from oauth2client.client import HttpAccessTokenRefreshError
 import sys
 import traceback
 
-example_client = Client.objects.get(id=1)       # @todo Get this from args
+clients = Client.objects.filter(keep_in_sync=True)
+
+print("Number of clients to keep in sync: {}".format(len(clients)))
 
 try:
-    sync_environment = SyncEnvironment(app_client=example_client)
-    sync_environment.sync()
+    for client in clients:
+        print(" -> Sync for client id: {} email domain: {}".format(client.id, client.email_domain))
+        sync_environment = SyncEnvironment(app_client=client)
+        sync_environment.sync()
 except HttpAccessTokenRefreshError as exc:
     print("Log This: Refresh Token is Failed for Client: {}, Exception Details: {}".format(example_client.id, exc))
 except Exception as exc:
