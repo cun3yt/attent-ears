5a581aca45b64e207ac9af93fbb1b0d391ec353d
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-08-08 01:04:29 -0700

    Redirect URL Correction for Outreach

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^logout$', views.sign_out, name='logout'),
     url(r'^settings$', views.settings, name='settings'),
-    url(r'^outreach-redirect$', views.outreach_redirect, name='outreach-redirect'),
+    url(r'^oauth/outreach-redirect$', views.outreach_redirect, name='outreach-redirect'),
     url(r'^$', views.index, name='index'),
 ]
