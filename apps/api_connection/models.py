2f5d42704f5ec37b1e43cd96a404d33845cd570f
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-08-10 11:56:17 -0700

    Outreach Initial Structure

--- apps/api_connection/models.py
+++ apps/api_connection/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from core.mixins import TimeStampedMixin
+from core.mixins import TimeStampedMixin, NoUpdateTimeStampedMixin
 from visualizer.models import User
 
 
@@ -13,3 +13,21 @@ class ApiConnection(TimeStampedMixin):
                              related_query_name='api_connection')
     type = models.CharField(max_length=30, default='')
     data = JSONField(default={})
+
+
+class ApiConnectionLog(NoUpdateTimeStampedMixin):
+    class Meta:
+        db_table = 'api_connection_log'
+
+    api_connection = models.ForeignKey(ApiConnection)
+    resource = models.CharField(max_length=50, default='')
+    args = JSONField(default={})
+    response = JSONField(default={})
+
+    @staticmethod
+    def log(api_connection: ApiConnection, resource, args, response):
+        new_log = ApiConnectionLog(api_connection=api_connection,
+                                   resource=resource,
+                                   args=args,
+                                   response=response)
+        new_log.save()
--- apps/outreach/models.py
+++ apps/outreach/models.py
@@ -1,31 +1,18 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
-from core.mixins import NoUpdateTimeStampedMixin, TimeStampedMixin
+from core.mixins import TimeStampedMixin
 from visualizer.models import User
 
 
-class OutreachUser(TimeStampedMixin):
+class OutreachProspect(TimeStampedMixin):
     class Meta:
-        db_table = 'outreach_user'
-
-    user = models.ForeignKey(User)
-    data = JSONField(default={})
+        db_table = 'outreach_prospect'
+    pass
 
 
-
-class OutreachApiLog(NoUpdateTimeStampedMixin):
+class OutreachMailing(TimeStampedMixin):
     class Meta:
-        db_table = 'outreach_api_log'
+        db_table = 'outreach_mailing'
+    pass
 
-    outreach_user = models.ForeignKey(OutreachUser)
-    resource = models.CharField(max_length=50, default='')
-    args = JSONField(default={})
-    response = JSONField(default={})
 
-    @staticmethod
-    def log(outreach_user: OutreachUser, resource, args, response):
-        new_log = OutreachApiLog(outreach_user=outreach_user,
-                                 resource=resource,
-                                 args=args,
-                                 response=response)
-        new_log.save()
