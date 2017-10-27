b5eb749c5e4f884835b35f924c3beeffa15ca718
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-10-27 17:51:55 +0300

    Trying Static Folder

--- ears/urls.py
+++ ears/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles import views
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
