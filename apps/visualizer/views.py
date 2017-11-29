a32c0856c8b8adea1f7d8207473627c8b7450440
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-11-29 21:02:00 +0300

    Removal of Unnecessary Comments

--- apps/visualizer/views.py
+++ apps/visualizer/views.py
@@ -22,38 +22,6 @@ def index(request):
 
     return render(request, 'landing-page.html', context=context)
 
-    # template = 'index.html'
-    #
-    # if not request.user.is_authenticated():
-    #     return render(request, template, context=context)
-    #
-    # client = request.user.client
-    #
-    # all_dashboards = client.periscopedashboard_set.filter(is_visible=True)
-    # dashboard_id = request.GET.get('dashboard_id', '')
-    #
-    # selected_dashboard_set = all_dashboards.filter(periscope_dashboard_id=dashboard_id)
-    #
-    # context['all_dashboards'] = all_dashboards
-    #
-    # if selected_dashboard_set.count() == 0:
-    #     context['no_dashboard_selected'] = True
-    #     return render(request, template, context=context)
-    #
-    # selected_dashboard = selected_dashboard_set[0]
-    #
-    # api_key = client.extra_info['periscope_api_key']
-    # url = selected_dashboard.get_embed_url(api_key)
-    #
-    # context['dashboards'] = client.periscopedashboard_set.filter(is_visible=True)
-    #
-    # context['selected_dashboard'] = {
-    #     'url': url,
-    #     'id': selected_dashboard.id
-    # }
-    #
-    # return render(request, template, context=context)
-
 
 @csrf_exempt
 def sign_out(request):
--- ears/settings.py
+++ ears/settings.py
@@ -94,10 +94,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ears.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'psqlextra.backend',
